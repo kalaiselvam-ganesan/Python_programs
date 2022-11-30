d9fa2773f1e5b167927c5b02eb3867026541d635
kalaiselvam-ganesan <114068214+kalaiselvam-ganesan@users.noreply.github.com>
2022-11-30 17:17:56 +0530

    Add files via upload

---
+++ function1.py
@@ -0,0 +1,13 @@
+# Function
+
+# function definition
+def my_function():
+    print("Hi, Welcome to my world")
+
+# function call
+my_function()
+
+
+#   output
+
+#   Hi, Welcome to my world
---
+++ function2.py
@@ -0,0 +1,18 @@
+# function
+
+# function definition
+def my_function(f_name):
+    print("Hi," + f_name +" " + "Welcome to my world!")
+
+# function call
+my_function("kalai")
+# argument passing to the function
+my_function("ram")
+my_function("sam")
+
+
+#   output
+
+#   Hi,kalai Welcome to my world! 
+#   Hi,ram Welcome to my world! 
+#   Hi,sam Welcome to my world! 
